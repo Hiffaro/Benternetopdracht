b9ba2f1414ccb8a8b851a8dcb9611168e38bbebb
Christophe van der Puijl <hiffaro@gmail.com>
2025-04-24 16:42:15 +0200

    More features.

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,71 @@
 #include "benternet.h"
+#include <algorithm>
 #include <nzmqt/nzmqt.hpp>
 #include <zmq.h>
 #include <QtCore>
 #include <QtNetwork>
 
-QByteArray doRoll(QString rollRequest) {
+QByteArray doRollVs(QString result, QString dc, QString ident) {
+    if(result.contains("Error")) {
+        qDebug() << "Upstream error.";
+        return result.toUtf8();
+    }
+
+    // Build return array.
+    QByteArray ret = QByteArray("dice>result>");
+    if(ident != "dice" && ident != "dicevs") ret += (ident + ">").toUtf8();
+
+    // Parse DC and result.
+    bool okVs = true;
+    auto vs = dc.toInt(&okVs);
+    bool okRes = true;
+    auto res = QString(result.split('>').last()).toInt(&okRes);
+
+    if(okVs && okRes) {
+        qDebug() << "Emitting:" << res << "vs" << vs;
+        return ret + QByteArray(res >= vs ? "pass" : "fail");
+    } else {
+        qDebug() << "Invalid DC.";
+        return ret + QByteArray("Error: Invalid DC.");
+    }
+}
+
+QByteArray doRoll(QString rollRequest, QString ident) {
     static QRandomGenerator rng = QRandomGenerator::securelySeeded();
-    try {
-        qDebug() << "Parsing roll request:" << rollRequest;
-        auto elements = rollRequest.split('d');
-        if(elements.length() == 2) {
-            bool okAmount = true;
-            auto amount = elements[0].toUInt(&okAmount);
-            bool okDSize = true;
-            auto dSize = elements[1].toUInt(&okDSize);
-            if(okAmount && okDSize) {
-                int total = 0;
-                for(int i = 0; i < amount; i++) total += rng.bounded(dSize) + 1;
-                qDebug() << "Emitting:" << total;
-                return QByteArray("dice>result>") + QByteArray::number(total);
-            } else {
-                qDebug() << "Invalid parameters.";
-                return QByteArray("dice>result>Error: DiceAmount and DiceSize must be positive integers.");
-            }
-        } else {
-            qDebug() << "Wrong format.";
-            return QByteArray("dice>result>Error: Expected format is [DiceAmount]d[DiceSize]. E.g.: 1d20.");
-        }
-    } catch(nzmqt::ZMQException &e) {
-        qDebug() << "ZMQException: " << e.what();
-        return QByteArray("dice>result>Internal service error.");
+
+    // Build return array.
+    QByteArray ret = QByteArray("dice>result>");
+    if(ident != "dice" && ident != "dicevs") ret += (ident + ">").toUtf8();
+
+    // Parse request.
+    qDebug() << "Parsing roll request:" << rollRequest;
+    QRegularExpression re(R"-(^(?<amount>\d+){,1}d(?<faces>\d+)(?<modifier>[+-]\d+){,1}(?<advdis>(adv)|(dis)){,1}$)-", QRegularExpression::CaseInsensitiveOption);
+    auto match = re.match(rollRequest);
+
+    if(match.hasMatch()) {
+        // Extract data from regex capture.
+        uint diceAmount = match.captured("amount").toUInt();
+        if(diceAmount == 0) diceAmount = 1;
+        uint diceFaces = match.captured("faces").toUInt();
+        bool keepHighest = match.captured("advdis") == "adv";
+        bool keepLowest = match.captured("advdis") == "dis";
+        int modifier = match.captured("modifier").toInt();
+
+        // Calculate two rolls, starting at modifier.
+        int total1 = modifier;
+        int total2 = modifier;
+        for(int i = 0; i < diceAmount; i++) total1 += rng.bounded(diceFaces) + 1;
+        for(int i = 0; i < diceAmount; i++) total2 += rng.bounded(diceFaces) + 1;
+
+        // Return the 1st, or the highest/lowest if that option was picked.
+        int total = total1;
+        if(keepHighest) total = std::max(total1, total2);
+        if(keepLowest)  total = std::min(total1, total2);
+        qDebug() << "Emitting:" << total;
+        return ret + QByteArray::number(total);
+    }  else {
+        qDebug() << "Wrong format.";
+        return ret + QByteArray("Error: Expected format is [DiceAmount]d[DiceSize](+/-[Modifier])([dis/adv]). E.g.: 1d20.");
     }
 }
 
@@ -44,13 +80,40 @@ int main(int argc, char **argv) {
     // Setup task forwarding.
     qDebug() << "Setting up message forwarding...";
     QObject::connect(benternet.data(), &Benternet::onReceive, [&](const QByteArray &message) {
-        QString rollRequest = QString(message.split('>').last());
-        benternet->send(doRoll(rollRequest));
+        auto params = QString(message).split(">");
+        if(params.size() >= 3) {
+            QString mode = params[1];
+            if(mode == "roll") {
+                QString rollRequest = QString(params[params.length() - 1]);
+                QString ident = QString(params[params.length() - 2]);
+                QByteArray result = doRoll(rollRequest, ident);
+                try {
+                    benternet->send(result);
+                } catch(nzmqt::ZMQException &e) {
+                    qDebug() << "ZMQException: " << e.what();
+                }
+            } else if(mode == "rollvs") {
+                QString rollRequest = QString(params[params.length() - 2]);
+                QString dc = QString(params[params.length() - 1]);
+                QString ident = QString(params[params.length() - 3]);
+                QByteArray result = doRollVs(doRoll(rollRequest, ident), dc, ident);
+                try {
+                    benternet->send(result);
+                } catch(nzmqt::ZMQException &e) {
+                    qDebug() << "ZMQException: " << e.what();
+                }
+            }
+        }
     });
 
     // Subscribe to our endpoints.
     qDebug() << "Subscribing to endpoint...";
+
+    // Roll in the format dice>roll>(identifier>)XdY±Z[dis/adv]. Returns result.
     benternet->subscribe("dice>roll>");
+
+    // Roll in the format dice>rollvs>(identifier>)XdY±Z[dis/adv]>DC. Returns true/false.
+    benternet->subscribe("dice>rollvs>");
 
     qDebug() << "Starting event loop...";
     return a.exec();
--- src/tester.cpp
+++ src/tester.cpp
@@ -1,12 +1,42 @@
 #include "benternet.h"
+#include <cstdlib>
 #include <nzmqt/nzmqt.hpp>
 #include <zmq.h>
 #include <QtCore>
 #include <QtNetwork>
 
+QByteArray generateRequest(QRandomGenerator *rng) {
+    bool addModifier = rng->bounded(2) == 1;
+    int addedModifier = rng->bounded(31) - 15;
+    int addedAdvDis = rng->bounded(3);
+    bool addDc = rng->bounded(2) == 1;
+    bool addIdent = rng->bounded(2) == 1;
+    int addedDc = rng->bounded(31);
+    int diceCount = rng->bounded(5) + 1;
+    QList<int> faces = {2, 3, 4, 6, 8, 10, 12, 20, 100};
+    int diceFaces = faces[rng->bounded(faces.size())];
+
+    QByteArray toSend = addDc ? QByteArray("dice>rollvs>") : QByteArray("dice>roll>");
+    if(addIdent) toSend += (QUuid::createUuid().toString(QUuid::WithoutBraces) + ">").toUtf8();
+    if(diceCount != 1) toSend += QByteArray::number(diceCount);
+    toSend += QByteArray("d");
+    toSend += QByteArray::number(diceFaces);
+    if(addModifier) {
+        toSend += addedModifier < 0 ? QByteArray("-") : QByteArray("+");
+        toSend += QByteArray::number(std::abs(addedModifier));
+    }
+    if(addedAdvDis == 1) toSend += QByteArray("adv");
+    if(addedAdvDis == 2) toSend += QByteArray("dis");
+    if(addDc) {
+        toSend += QByteArray(">");
+        toSend += QByteArray::number(addedDc);
+    }
+    return toSend;
+}
+
 int main(int argc, char **argv) {
     QCoreApplication a(argc, argv);
-    QRandomGenerator rng = QRandomGenerator::securelySeeded();
+    QRandomGenerator *rng = QRandomGenerator::global();
 
     // Setup benternet.
     qDebug() << "Starting benternet connection...";
@@ -16,10 +46,9 @@ int main(int argc, char **argv) {
     // Setup task forwarding.
     qDebug() << "Setting up response parsing...";
     QObject::connect(benternet.data(), &Benternet::onReceive, [&](const QByteArray &message) {
-        QString rollRequest = QString(message.split('>').last());
-        qDebug() << "Result:" << rollRequest;
-        QTimer::singleShot((rng.bounded(50) + 1) * 100, [&]() {
-            QByteArray toSend = QByteArray("dice>roll>") + QByteArray::number(rng.bounded(5) + 1) + QByteArray("d") + QByteArray::number(rng.bounded(100) + 1);
+        qDebug() << "Result:" << QString(message);
+        QTimer::singleShot((rng->bounded(50) + 1) * 100, [&]() {
+            auto toSend = generateRequest(rng);
             qDebug() << "Sending request:" << QString(toSend);
             benternet->send(toSend);
         });
@@ -30,7 +59,7 @@ int main(int argc, char **argv) {
     benternet->subscribe("dice>result>");
 
     qDebug() << "Starting event loop...";
-    QByteArray toSend = QByteArray("dice>roll>") + QByteArray::number(rng.bounded(5) + 1) + QByteArray("d") + QByteArray::number(rng.bounded(100) + 1);
+    auto toSend = generateRequest(rng);
     qDebug() << "Sending request:" << QString(toSend);
     benternet->send(toSend);
     return a.exec();
